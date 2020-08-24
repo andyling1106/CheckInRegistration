d2e0ef187ba1ff40b01dcc1f24796eb6b47c9c32
andyling1106 <69571636+andyling1106@users.noreply.github.com>
2020-08-24 14:52:38 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,44 @@
-  // Initialize Firebase
+
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyC-Tw4gWsCM_kbrxJ3D99ON2joCRyIhoGc",
-    authDomain: "contactform-28bc3.firebaseapp.com",
-    databaseURL: "https://contactform-28bc3.firebaseio.com",
-    projectId: "contactform-28bc3",
-    storageBucket: "contactform-28bc3.appspot.com",
-    messagingSenderId: "56744531715",
-    appId: "1:56744531715:web:2b9313cc2df5887464fb36"
+    apiKey: "AIzaSyBajmTv95bdeGcMkfniNzNmz7_ekW7XGzo",
+    authDomain: "clockme-8430c.firebaseapp.com",
+    databaseURL: "https://clockme-8430c.firebaseio.com",
+    projectId: "clockme-8430c",
+    storageBucket: "clockme-8430c.appspot.com",
+    messagingSenderId: "774557927646",
+    appId: "1:774557927646:web:74ec3bdb8c4ba79ff51a4f",
+    measurementId: "G-0KFB9H62EK"
   };
+  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 
 // reference messages collection from firebase database 
-var messagesRef = firebase.database().ref('mess');
+var messagesRef = firebase.database().ref('clockme');
 
-document.getElementById('contactForm').addEventListener('submit', submitForm);
+document.getElementById('clockme').addEventListener('submit', submitForm);
 
 // Submit Form
 function submitForm(e){
     e.preventDefault();
  
-  // get value
-  var name = getInputVal('name');
-  var phone = getInputVal('phone');
-  var bodytemp  = getInputVal('bodytemp');
 
-             
+
+  // get value
+  var passno = getInputVal('passno');
+  var name = getInputVal('name');
+  var latitude = getInputVal("latitude");
+  var longtitude = getInputVal("longtitude");
+   
   var d = new Date();
   var cdate = d.toLocaleString(); 
   
+
+
   // Save Message
-  saveMessage(name, phone, bodytemp, cdate);
+  saveMessage(passno,name,cdate,latitude,longtitude);
 
 
 // show alert  'block = display'
@@ -43,7 +50,7 @@ setTimeout(function() {
 }, 3000);
 
 //clear form 
-  document.getElementById('contactForm').reset();
+  document.getElementById('clockme').reset();
 }
 
 //function to get form value
@@ -61,19 +68,24 @@ var onComplete = function(error) {
 };
 
 
-//Save messages to firebase
-function saveMessage(name,phone,bodytemp,cdate){
+  //Save messages to firebase
+function saveMessage(passno,name,cdate,latitude,longtitude){
   var newMessageRef = messagesRef.push();
   newMessageRef.set({
+    passno: passno,
       name: name,
-      phone: phone,
-      bodytemp: bodytemp,
-      cdate: cdate,
-        }, onComplete);
-     
-        location.replace("form_submit.html");
+        cdate: cdate,
+        latitude: latitude,
+        longtitude: longtitude,
+                         }, onComplete);
+             location.replace("form_submit.html");
       
 }
+
+
+
+
+
 
 
 
